d0a61117e3fcbe100f81a3731182aab0bcd66334
drmsbgr <drmsbgr@gmail.com>
2025-01-16 17:49:50 +0300

    dto remove

--- BookApi/Entities/Dtos/BookAuthorDto.cs
+++
@@ -1,9 +0,0 @@
-namespace BookApi.Entities.Dtos;
-
-public record BookAuthor
-{
-    public int AuthorId { get; init; }
-    public int BookId { get; init; }
-    public BookDto Book { get; set; }
-    public AuthorDto Author { get; set; }
-}
